f2540cd1adf395c372c84060e152f3f8c9a43eb4
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-25 18:11:27 +0200

    added Searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,22 @@
+import css from './Searchbar.module.css';
+
+export const Searchbar = ({ onSubmit }) => {
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={onSubmit}>
+        <button type="submit" className={css.button}>
+          <span className={css.button_label}>Search</span>
+        </button>
+
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="searchbar"
+        />
+      </form>
+    </header>
+  );
+};
