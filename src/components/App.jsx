d8f6128bc343e43c3fbe532188c16ca153f40672
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-26 01:25:55 +0200

    updated App
    -changed Spinner
    -updated state
    -added events func for Modal and ImageItem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import { Component } from 'react';
-
-import { Audio } from 'react-loader-spinner';
+import { Blocks } from 'react-loader-spinner';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { LoadMoreButton } from './Button/Button';
 import { Modal } from './Modal/Modal';
-
 
 //key for API
 const key = '32145411-8f2f66f9688645c1a0ae56fcc';
@@ -18,18 +16,27 @@ export class App extends Component {
     currentPage: 1,
     images: [],
     isLoading: false,
+    modalURL: '',
+    isModalOpen: false,
   };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
 
   fetchAPI = async (q, page) => {
     try {
-      this.setState({isLoading: true})
+      this.setState({ isLoading: true });
       const response = await fetch(apiURL + `&q=${q}&page=${page}`);
       const data = await response.json();
       const images = data.hits;
       this.setState(prevState => ({
         images: prevState.images.concat(images),
       }));
-      // this.setState({images})
     } catch (error) {
       this.setState({ error });
       console.log(error);
@@ -40,18 +47,15 @@ export class App extends Component {
     e.preventDefault();
     const searchbarValue = e.currentTarget.elements.searchbar.value;
     this.setState({ currentPage: 1, images: [], currentQuery: searchbarValue });
-    // const { currentPage } = this.state;
-
-    // this.setState({ currentQuery: searchbarValue });
     await this.fetchAPI(searchbarValue, 1);
-    this.setState({isLoading: false})
+    this.setState({ isLoading: false });
   };
 
   fetchMoreImages = () => {
     const { currentQuery, currentPage } = this.state;
     this.fetchAPI(currentQuery, currentPage + 1);
     this.handleCurrentPageUpdate();
-    this.setState({isLoading: false})
+    this.setState({ isLoading: false });
   };
 
   handleCurrentPageUpdate = () => {
@@ -60,39 +64,54 @@ export class App extends Component {
     });
   };
 
-  // handleClick = (e) => {
-  //   console.log("danke")
-  //   console.log(e.target);
-  //   this.setState({isModalOpen: true})
-  // }
+  openModal = e => {
+     if (e.target.nodeName !== 'IMG') {
+       return;
+     }
+    const srcset = e.target.srcset;
+    this.setState({ isModalOpen: true, modalURL: srcset }, () => {
+      console.log(this.state);
+    });
+  };
+
+  closeModal = () => {
+    this.setState({ isModalOpen: false, modalURL: '' });
+  };
+
+  handleKeyPress = e => {
+    if (e.key === 'Escape') {
+      this.closeModal();
+    }
+  };
 
   render() {
-
+    const { isLoading, images, isModalOpen, modalURL } = this.state;
     return (
-      <div>
+      <div
+        styles={{
+          display: 'grid',
+          gridTemplateColumns: '1fr',
+          gridGap: '16px',
+          paddingBottom: '24px',
+        }}
+      >
         <Searchbar onSubmit={this.handleSubmit}></Searchbar>
-        {!this.state.isLoading ? (
-          <ImageGallery onClick={this.handleClick}>
-            <ImageGalleryItem
-              loadedPhotos={this.state.images}
-            ></ImageGalleryItem>
+        {!isLoading ? (
+          <ImageGallery onClick={this.openModal}>
+            <ImageGalleryItem loadedPhotos={images}></ImageGalleryItem>
           </ImageGallery>
         ) : (
-          <Audio
-            height="80"
-            width="80"
-            radius="9"
-            color="green"
-            ariaLabel="three-dots-loading"
-            wrapperStyle
-            wrapperClass
-          />
+          <Blocks/>
         )}
         <LoadMoreButton
-          showButton={this.state.images.length > 0}
+          showButton={images.length > 0}
           onClick={this.fetchMoreImages}
         ></LoadMoreButton>
-        {/* <Modal show={this.state.isModalOpen} imageUrl={}></Modal> */}
+        <Modal
+          show={isModalOpen}
+          imageUrl={modalURL}
+          onClose={this.closeModal}
+        ></Modal>
       </div>
     );
   }
