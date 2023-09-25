0d3a5e0a6b4c9eada843e9c16ee7f42288b3ce7e
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-25 23:48:48 +0000

    Deploying to gh-pages from @ martadomzalska/goit-hw-image-finder@441a0c8dec1bdc05e1809c6de3e4b902e30c1697 🚀

--- src/components/ImageGallery/ImageGallery.jsx
+++
@@ -1,5 +0,0 @@
-import css from './ImageGallery.module.css';
-
-export const ImageGallery = ({ children , onClick}) => {
-    return <ul onClick={onClick} className={css.gallery}>{children}</ul>;
-};
