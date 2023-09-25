7fe982844f4e28faf2d9f9238c96c5580546efbf
martadomzalska <105391131+martadomzalska@users.noreply.github.com>
2023-09-26 01:24:37 +0200

    added Modal+ styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,19 @@
 import { Component } from 'react';
+import css from './Modal.module.css';
 
 export class Modal extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     if (!this.props.show) {
       return null;
     }
 
     return (
-      <div class="overlay">
-        <div class="modal" onClick={this.props.onClose}>
-           <img src={this.props.imageUrl} alt="Modal" />
+      <div className={css.overlay} onClick={this.props.onClose}>
+        <div className={css.modal}>
+          <img src={this.props.imageUrl} alt="Modal" width="800" height="600" />
         </div>
       </div>
     );
